3063b11ef1c7d3239d5c53f2e823f11509fcba4b
Dan <baghdadi.danny@gmail.com>
2022-04-25 02:41:32 -0700

    init

---
+++ app.js
@@ -0,0 +1,264 @@
+const mapData = {
+  minX: 1,
+  maxX: 14,
+  minY: 4,
+  maxY: 12,
+  blockedSpaces: {
+    "7x4": true,
+    "1x11": true,
+    "12x10": true,
+    "4x7": true,
+    "5x7": true,
+    "6x7": true,
+    "8x6": true,
+    "9x6": true,
+    "10x6": true,
+    "7x9": true,
+    "8x9": true,
+    "9x9": true,
+  },
+};
+
+// Options for Player Colors... these are in the same order as our sprite sheet
+const playerColors = ["green", "blue", "teal", "yellow", "red", "purple"];
+
+//Misc Helpers
+function randomFromArray(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
+function getKeyString(x, y) {
+  return `${x}x${y}`;
+}
+
+function createName() {
+  const prefix = randomFromArray([
+    "COOL",
+    "SUPER",
+    "HIP",
+    "SMUG",
+    "COOL",
+    "SILKY",
+    "GOOD",
+    "SAFE",
+    "DEAR",
+    "DAMP",
+    "WARM",
+    "RICH",
+    "LONG",
+    "DARK",
+    "SOFT",
+    "BUFF",
+    "DOPE",
+  ]);
+  const animal = randomFromArray([
+    "BEAR",
+    "DOG",
+    "CAT",
+    "FOX",
+    "LAMB",
+    "LION",
+    "BOAR",
+    "GOAT",
+    "VOLE",
+    "SEAL",
+    "PUMA",
+    "MULE",
+    "BULL",
+    "BIRD",
+    "BUG",
+  ]);
+  return `${prefix} ${animal}`;
+}
+
+function isSolid(x,y) {
+
+  const blockedNextSpace = mapData.blockedSpaces[getKeyString(x, y)];
+  return (
+    blockedNextSpace ||
+    x >= mapData.maxX ||
+    x < mapData.minX ||
+    y >= mapData.maxY ||
+    y < mapData.minY
+  )
+}
+
+function getRandomSafeSpot() {
+  //We don't look things up by key here, so just return an x/y
+  return randomFromArray([
+    { x: 1, y: 4 },
+    { x: 2, y: 4 },
+    { x: 1, y: 5 },
+    { x: 2, y: 6 },
+    { x: 2, y: 8 },
+    { x: 2, y: 9 },
+    { x: 4, y: 8 },
+    { x: 5, y: 5 },
+    { x: 5, y: 8 },
+    { x: 5, y: 10 },
+    { x: 5, y: 11 },
+    { x: 11, y: 7 },
+    { x: 12, y: 7 },
+    { x: 13, y: 7 },
+    { x: 13, y: 6 },
+    { x: 13, y: 8 },
+    { x: 7, y: 6 },
+    { x: 7, y: 7 },
+    { x: 7, y: 8 },
+    { x: 8, y: 8 },
+    { x: 10, y: 8 },
+    { x: 8, y: 8 },
+    { x: 11, y: 4 },
+  ]);
+}
+
+
+(function () {
+
+  let playerId;
+  let playerRef;
+  let players = {};
+  let playerElements = {};
+
+  const gameContainer = document.querySelector(".game-container");
+  const playerNameInput = document.querySelector("#player-name");
+  const playerColorButton = document.querySelector("#player-color");
+
+
+  function handleArrowPress(xChange=0, yChange=0) {
+    const newX = players[playerId].x + xChange;
+    const newY = players[playerId].y + yChange;
+    if (!isSolid(newX, newY)) {
+      //move to the next space
+      players[playerId].x = newX;
+      players[playerId].y = newY;
+      if (xChange === 1) {
+        players[playerId].direction = "right";
+      }
+      if (xChange === -1) {
+        players[playerId].direction = "left";
+      }
+      playerRef.set(players[playerId]);
+    }
+  }
+
+  function initGame() {
+
+    new KeyPressListener("ArrowUp", () => handleArrowPress(0, -1))
+    new KeyPressListener("ArrowDown", () => handleArrowPress(0, 1))
+    new KeyPressListener("ArrowLeft", () => handleArrowPress(-1, 0))
+    new KeyPressListener("ArrowRight", () => handleArrowPress(1, 0))
+
+    const allPlayersRef = firebase.database().ref(`players`);
+
+    allPlayersRef.on("value", (snapshot) => {
+      //Fires whenever a change occurs
+      players = snapshot.val() || {};
+      Object.keys(players).forEach((key) => {
+        const characterState = players[key];
+        let el = playerElements[key];
+        // Now update the DOM
+        el.querySelector(".Character_name").innerText = characterState.name;
+        el.setAttribute("data-color", characterState.color);
+        el.setAttribute("data-direction", characterState.direction);
+        const left = 16 * characterState.x + "px";
+        const top = 16 * characterState.y - 4 + "px";
+        el.style.transform = `translate3d(${left}, ${top}, 0)`;
+      })
+    })
+    allPlayersRef.on("child_added", (snapshot) => {
+      //Fires whenever a new node is added the tree
+      const addedPlayer = snapshot.val();
+      const characterElement = document.createElement("div");
+      characterElement.classList.add("Character", "grid-cell");
+      if (addedPlayer.id === playerId) {
+        characterElement.classList.add("you");
+      }
+      characterElement.innerHTML = (`
+        <div class="Character_shadow grid-cell"></div>
+        <div class="Character_sprite grid-cell"></div>
+        <div class="Character_name-container">
+          <span class="Character_name"></span>
+        </div>
+        <div class="Character_you-arrow"></div>
+      `);
+      playerElements[addedPlayer.id] = characterElement;
+
+      //Fill in some initial state
+      characterElement.querySelector(".Character_name").innerText = addedPlayer.name;
+      characterElement.setAttribute("data-color", addedPlayer.color);
+      characterElement.setAttribute("data-direction", addedPlayer.direction);
+      const left = 30 * addedPlayer.x + "px";
+      const top = 30 * addedPlayer.y - 8 + "px";
+      characterElement.style.transform = `translate3d(${left}, ${top}, 0)`;
+      gameContainer.appendChild(characterElement);
+    })
+
+
+    //Remove character DOM element after they leave
+    allPlayersRef.on("child_removed", (snapshot) => {
+      const removedKey = snapshot.val().id;
+      gameContainer.removeChild(playerElements[removedKey]);
+      delete playerElements[removedKey];
+    })
+
+    //Updates player name with text input
+    playerNameInput.addEventListener("change", (e) => {
+      const newName = e.target.value || createName();
+      playerNameInput.value = newName;
+      playerRef.update({
+        name: newName
+      })
+    })
+
+    //Update player color on button click
+    playerColorButton.addEventListener("click", () => {
+      const mySkinIndex = playerColors.indexOf(players[playerId].color);
+      const nextColor = playerColors[mySkinIndex + 1] || playerColors[0];
+      playerRef.update({
+        color: nextColor
+      })
+    })
+
+  }
+
+  firebase.auth().onAuthStateChanged((user) => {
+    console.log(user)
+    if (user) {
+      //You're logged in!
+      playerId = user.uid;
+      playerRef = firebase.database().ref(`players/${playerId}`);
+
+      const name = createName();
+      playerNameInput.value = name;
+
+      const {x, y} = getRandomSafeSpot();
+
+
+      playerRef.set({
+        id: playerId,
+        name,
+        direction: "right",
+        color: randomFromArray(playerColors),
+        x,
+        y
+      })
+
+      //Remove me from Firebase when I diconnect
+      playerRef.onDisconnect().remove();
+
+      //Begin the game now that we are signed in
+      initGame();
+    } else {
+      //You're logged out.
+    }
+  })
+
+  firebase.auth().signInAnonymously().catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ...
+    console.log(errorCode, errorMessage);
+  });
+
+
+})();
