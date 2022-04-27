eb05720c5b0ed3c3c922082cbd9a126d4673502d
Dan <baghdadi.danny@gmail.com>
2022-04-26 23:00:51 -0700

    Restructured Assets
    Assets have been moved around.

--- js/game.js
+++ js/game.js
@@ -10,12 +10,11 @@ Game.init = function(){
 
 //load game assets
 Game.preload = function() {
-    game.load.tilemap('map', 'assets/map/example_map.json', null, Phaser.Tilemap.TILED_JSON);
-    game.load.spritesheet('tileset', 'assets/map/tilesheet.png',32,32);
-    game.load.image('sprite','assets/sprites/frog.png');
-    game.load.image('frog_body','assets/sprites/frog_body.png');
-    game.load.image('frog_belly','assets/sprites/frog_belly.png');
-    game.load.image('frog_eyes','assets/sprites/frog_eyes.png');
+    game.load.tilemap('map', 'assets/room/house_on_the_river/example_map.json', null, Phaser.Tilemap.TILED_JSON);
+    game.load.spritesheet('tileset', 'assets/room/house_on_the_river/tilesheet.png',32,32);
+    game.load.image('frog_body','assets/character/frog_body.png');
+    game.load.image('frog_belly','assets/character/frog_belly.png');
+    game.load.image('frog_eyes','assets/character/frog_eyes.png');
 };
 
 //set up game
