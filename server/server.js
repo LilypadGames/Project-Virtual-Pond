d308009c9034bef48211c3c9dd558992678d3350
Dan <baghdadi.danny@gmail.com>
2022-05-12 23:18:50 -0700

    Twitch Authentication
    New Auth page, Ability to grab player ID and Display Name from their Twitch account.

--- server/server.js
+++ server/server.js
@@ -1,32 +1,164 @@
 // Handles Server
 
-//frameworks
-var express = require('express');
-var app = express();
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
-
 //imports
 const util = require(__dirname + '/utility');
-const config = require(__dirname + '/config/config');
 
-//send client files (html/css/js/assets)
-app.use('/',express.static(__dirname + '/../client'));
+//dependency: File Parsing
+const fs = require('fs');
+const path = require('path');
 
-//send main HTML file to client
-app.get('/',function(req, res){
-    res.sendFile('index.html', { root: 'client/html' });
+//get config values
+const config = JSON.parse(fs.readFileSync(path.join(__dirname, '/config/config.json')));
+
+//dependency: web server
+var express        = require('express');
+var session        = require('express-session');
+
+//init web server
+var app = express();
+var server = require('http').Server(app);
+
+//dependency: websocket
+var io = require('socket.io')(server);
+
+//dependency: database
+var firebase = require("firebase-admin");
+
+//init database
+firebase.initializeApp({
+    credential: firebase.credential.cert(config.firebase),
+    databaseURL: "https://project-virtual-pond-default-rtdb.firebaseio.com"
 });
+var database = firebase.database();
+
+//dependency: authentication
+var passport       = require('passport');
+var OAuth2Strategy = require('passport-oauth').OAuth2Strategy;
+var request        = require('request');
+var handlebars     = require('handlebars');
+
+//init authentication
+app.use(session({secret: config.twitch.sessionSecret, resave: false, saveUninitialized: false}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+//serve client files (html/css/js/assets)
+app.use('/', express.static(__dirname + '/../client'));
+
+
+////// AUTHENTICATION
+
+//override passport profile function to get user profile from Twitch API
+OAuth2Strategy.prototype.userProfile = function(accessToken, done) {
+
+    var options = {
+        url: 'https://api.twitch.tv/helix/users',
+        method: 'GET',
+        headers: {
+            'Client-ID': config.twitch.clientID,
+            'Accept': 'application/vnd.twitchtv.v5+json',
+            'Authorization': 'Bearer ' + accessToken
+        }
+    };
+
+    request(options, function (error, response, body) {
+        if (response && response.statusCode == 200) {
+            done(null, JSON.parse(body));
+        }
+        else {
+            done(JSON.parse(body));
+        };
+    });
+};
+
+passport.serializeUser(function(user, done) {
+    done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+    done(null, user);
+});
+
+//get user info
+var twitchName;
+var twitchID;
+var twitchImage;
+
+passport.use('twitch', new OAuth2Strategy({
+
+    authorizationURL: 'https://id.twitch.tv/oauth2/authorize',
+    tokenURL: 'https://id.twitch.tv/oauth2/token',
+    clientID: config.twitch.clientID,
+    clientSecret: config.twitch.clientSecret,
+    callbackURL: config.twitch.callbackURL,
+    state: true
+    },
+
+    function(accessToken, refreshToken, profile, done) {
+        profile.accessToken = accessToken;
+        profile.refreshToken = refreshToken;
+
+        //get user info
+        twitchName = profile.data[0].display_name;
+        twitchID = profile.data[0].id;
+        twitchImage = profile.data[0].profile_image_url;
+
+        //store user info in firebase
+        // database.ref('users').set()
+        // User.findOrCreate(..., function(err, user) {
+        //  done(err, user);
+        // });
+
+        done(null, profile);
+    }
+));
+
+//set route to start OAuth link, this is where you define scopes to request
+app.get('/auth/twitch', passport.authenticate('twitch', { scope: 'user_read' }));
+
+//set route for OAuth redirect
+app.get('/auth/twitch/callback', passport.authenticate('twitch', { successRedirect: '/', failureRedirect: '/' }));
+
+// // Define a simple template to safely generate HTML with values from user's profile
+// var template = handlebars.compile(`
+// <html><head><title>Twitch Auth Sample</title></head>
+// <table>
+//     <tr><th>Access Token</th><td>{{accessToken}}</td></tr>
+//     <tr><th>Refresh Token</th><td>{{refreshToken}}</td></tr>
+//     <tr><th>Display Name</th><td>{{twitchID}}</td></tr>
+//     <tr><th>Bio</th><td>{{twitchName}}</td></tr>
+//     <tr><th>Image</th><td>{{twitchImage}}</td></tr>
+// </table></html>`);
+
+//detect authentication and serve game page
+app.get('/', function (req, res) {
+
+    //successfully authenticated
+    if (req.session && req.session.passport && req.session.passport.user) {
+        // res.send(template(req.session.passport.user));
+        res.sendFile('index.html', { root: 'client/html' });
+    }
+
+    //request authentication
+    else {
+        res.sendFile('auth.html', { root: 'client/html' });
+    }
+});
+
+//////
+
+//init web server
+server.listen(process.env.PORT || config.server.port, function () {
+    console.log(util.timestampString('Listening on port ' + server.address().port));
+});
+
+
+////// WEBSOCKETS (Socket.io)
 
 //init ID
 server.lastPlayerID = 0;
 
-//init server
-server.listen(process.env.PORT || config.server.port,function(){
-    console.log(util.timestampString('Listening on Port: '+server.address().port));
-});
-
-//on new connection
+//on new websocket connection
 io.on('connection', async function(socket){
 
     //triggers on new player loading the world
@@ -36,18 +168,16 @@ io.on('connection', async function(socket){
         socket.player = {
 
             //generate ID
-            id: server.lastPlayerID++,
-
-            name: 'DanMizu',
+            id: twitchID,
 
             //generate starting location
             x: util.getRandomInt(0, 24 * 32),
             y: util.getRandomInt(0, 17 * 32),
 
-            // //set direction
-            // direction: 'right',
+            //get name
+            name: twitchName,
 
-            //set tint
+            //get tint
             tint: Math.random() * 0xffffff
         };
 
@@ -142,3 +272,5 @@ async function getAllPlayers(){
     //return list of connected players
     return connectedPlayers;
 }
+
+//////
