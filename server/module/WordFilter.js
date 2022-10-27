31ff42d476564341a510b6a6d20a1053989ff028
Dan <baghdadi.danny@gmail.com>
2022-10-27 04:03:08 -0700

    upgraded word filter

--- server/module/WordFilter.js
+++ server/module/WordFilter.js
@@ -20,10 +20,68 @@ export default {
         });
 
         //add phonetic blacklist
-        this.wordData.blacklist = this.wordData.blacklist.concat(wordDataSource.blacklistPhonetic);
+        this.wordData.blacklist = this.wordData.blacklist.concat(
+            wordDataSource.blacklistPhonetic
+        );
     },
 
     get: function (listType) {
         return this.wordData[listType];
+    },
+
+    convertMessage: function (message, split = true) {
+        //remove and convert non-alphabetical characters
+        const messageFormatted = message
+            .replace('@', 'a')
+            .replace('0', 'o')
+            .replace('1', 'i')
+            .replace('5', 's')
+            .replace(/[^0-9a-z ]/gi, '');
+
+        //convert each word
+        let messagePhonetics;
+        if (split) {
+            //split by spaces
+            let messageSplit = messageFormatted.split(' ');
+
+            //convert words
+            messagePhonetics = [];
+            messageSplit.forEach((string) => {
+                messagePhonetics.push(Metaphone.process(string));
+            });
+        } else {
+            //convert words
+            messagePhonetics = Metaphone.process(messageFormatted);
+        }
+
+        return messagePhonetics;
+    },
+
+    checkMessage: function (message) {
+        //convert message to phonetic version
+        let messagePhonetics = this.convertMessage(message);
+
+        //compare blacklisted words to words in message using phonetics
+        let blacklist = this.wordData.blacklist;
+        console.log(blacklist);
+        console.log(messagePhonetics);
+        let noSlur = true;
+        blacklist.every((phonetic) => {
+            //find blacklisted words
+            messagePhonetics.every((word) => {
+                if (phonetic === word) {
+                    noSlur = false;
+                    return false;
+                }
+            });
+
+            //if match found, dont look through the rest
+            if (!noSlur) return false;
+
+            //continue
+            return true;
+        });
+
+        return noSlur;
     },
 };
