90288ffb775c8f67e745b46250e942dccc141d57
Dan <baghdadi.danny@gmail.com>
2023-04-18 09:47:48 -0700

    install rexrainbow ui plugin for phaser

--- client/src/scene/overlay/UI.ts
+++ client/src/scene/overlay/UI.ts
@@ -1,6 +1,10 @@
 import Phaser from "phaser";
+import InputText from "phaser3-rex-plugins/plugins/inputtext.js";
+import ColorScheme from "../../utility/ColorScheme";
 
 export default class UI extends Phaser.Scene {
+	chatbox!: InputText;
+
 	constructor() {
 		super({ key: "UI" });
 	}
@@ -10,5 +14,42 @@ export default class UI extends Phaser.Scene {
 	preload() {}
 
 	create() {
+		// create chatbox
+		this.chatbox = new InputText(this, {
+			id: "chat-box",
+			x: this.sys.game.canvas.width / 2,
+			y: this.sys.game.canvas.height - this.sys.game.canvas.height / 23,
+			width: this.sys.game.canvas.width * 0.6,
+			height: this.sys.game.canvas.width / 27,
+			placeholder: "Say Yo...",
+			color: utility.hexIntegerToString(ColorScheme.Black),
+			maxLength: this.messageMaxLength,
+			depth: this.depthUI,
+			onFocus: (inputBox) => {
+				if (this.menuOpen) inputBox.setBlur();
+			},
+			onKeydown: (inputBox, event) => {
+				if (event.key == "Enter") {
+					//format message
+					const chatMessage = inputBox.text
+						.substr(0, this.messageMaxLength)
+						.trim()
+						.replace(/\s+/g, " ");
+
+					//send the message to the server
+					if (chatMessage !== "" && chatMessage !== null) {
+						client.playerSendingMessage(chatMessage);
+					}
+
+					//leave chat bar
+					else {
+						inputBox.setBlur();
+					}
+
+					//clear chat box
+					inputBox.setText("");
+				}
+			},
+		});
 	}
 }
